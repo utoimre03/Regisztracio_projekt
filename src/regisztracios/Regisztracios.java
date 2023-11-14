31b5840fc753dbd3ab531a405ebac77910196d43
Ütő Imre (SZF_2023_2025) <uto.imre@szamalk-szalezi.hu>
2023-11-14 13:38:37 +0100

    gitalap

---
+++ src/regisztracios/Regisztracios.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package regisztracios;
+
+/**
+ *
+ * @author ÜtőImre(SZOFT_2023_2
+ */
+public class Regisztracios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
